436924c808e17c1c98561367f5d2fb3f11e2af23
Ruth <ruro122020@gmail.com>
2023-10-25 16:32:27 -0400

    fetch plants fromt the server

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,9 @@
+/*
+  1. When the app starts, I can see all plants.
+  2. I can add a new plant to the page by submitting the form.
+  3. I can mark a plant as "sold out".
+  4. I can search for plants by their name and see a filtered list of plants.
+*/
 import React from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,9 +1,45 @@
-import React from "react";
+/*
+  In the PlantPage component:
+    -create a plants state variable and set it to be an array
+    -fetch for the plants data in a useEffect hook
+    -set the plants list to the plants state variable
+    -pass the plants array to the PlantPage component
+  In the PlantList component:
+    -iterate through the plants array and display all the plants to the DOM 
+    -when it's time to add a new plant fromt he NewPlantForm component:
+        --create a function to pass down as props to the NewPlantForm component
+          ---In this function, add the new plant object to the plants array
+  In the NewPlantForm component:
+    -create a plant state and set it to an object:
+      --object format:
+        {
+          name:'',
+          image:'',
+          price: ''
+        }
+    - add a value attribut to the inputs and set the value to match the properties of the plant object
+    - create an handleInputChange function 
+        -- In this function, get the name of the input and the value of the input
+        -- set the plant object to be the name and value of the inputs
+    - add an onChange event to each input and set the callback to be the handleInputChange function
+    - create a handleSubmit function 
+        -- In this function, add the plant object to the server 
+        -- call the function from the PlantList component
+*/
+import React,{useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plantsList, setPlantsList] = useState([])
+
+  //fetch requests
+  useEffect(()=>{
+    fetch(' http://localhost:6001/plants')
+    .then(res => res.json())
+    .then(plants => console.log(plants))
+  }, [])
   return (
     <main>
       <NewPlantForm />
