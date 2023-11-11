9e6c9068b09012bd785126f8214be7453405e2b7
Ruth <ruro122020@gmail.com>
2023-11-10 20:15:32 -0500

    clean up code &&all deliverable complete

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,12 @@ Core Deliverables:
   2. I can add a new plant to the page by submitting the form.
   3. I can mark a plant as "sold out".
   4. I can search for plants by their name and see a filtered list of plants.
-  Core deliverable are done
+  Core deliverables are done
 
   Advanced Deliverable:
-  1. I can update the price of a plant and still see the updated price after refreshing the page.(done)
-  2. I can delete a plant and it is still gone when I refresh the page.(done)
+  1. I can update the price of a plant and still see the updated price after refreshing the page.
+  2. I can delete a plant and it is still gone when I refresh the page.
+  Advanced delierables are done
 */
 import React from "react";
 import Header from "./Header";
